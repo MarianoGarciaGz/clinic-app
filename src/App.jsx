34e2477ddd6de60c87dacff44818dee3a1f4c822
Andrea_Aguilar <l20120949@morelia.tecnm.mx>
2023-11-14 12:52:07 -0600

    Avance pag contacto

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import logo from './logo.svg'
 import './App.css'
 import Navlink from './Routes/Navlink';
 import { BrowserRouter } from 'react-router-dom'
+import Contacto from './pages/Contacto'
+
 
 function App() {
     return (
+        
         <Navlink />
+        
     )
 }
 
--- src/Routes/Navlink.jsx
+++ src/Routes/Navlink.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Home from '../pages/Home'
 import Agendar from '../pages/Agendar'
+import Contacto from '../pages/Contacto'
 
 const Navlink = () => {
     return (
@@ -10,6 +11,7 @@ const Navlink = () => {
                 <Route path='/'>
                     <Route index element={<Home />} />
                     <Route path='agendar' element={<Agendar />} />
+                    <Route path='contacto' element={<Contacto />} /> 
                 </Route>
             </Routes>
         </BrowserRouter>
