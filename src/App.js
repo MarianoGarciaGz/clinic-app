98ae4adfba8303cfb03751dd0cad7c75bd856d35
OscarFuentes2027 <oscarfuentes2027@gmail.com>
2023-11-15 01:12:58 -0600

    database

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css'
 import Home from './pages/Home'
 import Agendar from './pages/Agendar'
 
+
+
 function App() {
     return (
         <div className="App">
             <Home />
+            <Agendar />
         </div>
     )
 }
+
 
 export default App
--- src/components/atoms/ContentElegir.jsx
+++ src/components/atoms/ContentElegir.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 
 function ContentElegir({ concepto }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="content-elegir">
       <div className="input-container">
         <label className='labelcitas'>
           {concepto}
-          <input className="rounded-box-elegir" type="text" name={concepto} />
+          <input
+            className="rounded-box-elegir"
+            type="text"
+            name={concepto}
+            value={inputValue}
+            onChange={handleInputChange}
+          />
         </label>
-        {/* <div className="submit-button">
-        <input type="submit" value="AGREGAR" />
-        </div> */}
-        
       </div>
     </div>
   );
 }
-
 export default ContentElegir;
