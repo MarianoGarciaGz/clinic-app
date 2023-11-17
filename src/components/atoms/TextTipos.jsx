854bcfa2f175bc32b95ae874bba4ee8e60e4ef36
Mariano García <marianogarciagz@gmail.com>
2023-11-17 00:18:58 -0600

    add hyphens-none

--- src/components/atoms/TextTipos.jsx
+++ src/components/atoms/TextTipos.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function TextTipos({ text }) {
-  return (
-    <div className="text-component-tipos">
-      {text}
-    </div>
-  );
+	return <div className="text-component-tipos hyphens-none">{text}</div>
 }
 
-export default TextTipos;
+export default TextTipos
--- src/components/organism/About.jsx
+++ src/components/organism/About.jsx
@@ -7,40 +7,35 @@ import image3 from '../Images/producto.png'
 const About = () => {
 	return (
 		<section className="About">
-			<Heading className="About-h2" title="¿Quiénes somos?" />
-			<div className="row g-4 px-5 row-cols-1 row-cols-lg-3 m-5">
-				<div className="col d-flex align-items-start px-5">
-					<div className="text-body-emphasis d-inline-flex align-items-center justify-content-center flex-shrink-0 me-3">
+			<Heading className="About-h2" title="¿Quiénes somos?"></Heading>
+			<div class="row g-4 px-5 row-cols-1 row-cols-lg-3 my-5 mx-auto container">
+				<div class="col d-flex align-items-start px-5">
+					<div class="text-body-emphasis d-inline-flex align-items-center justify-content-center flex-shrink-0 me-3">
 						<img className="About-img" src={image1} alt="" />
 					</div>
-					<div className="vertical-line mx-3 align-self-stretch"></div>
-					<div className="d-flex flex-column justify-content-start">
-						<h3 className="About-h3">GARANTÍA</h3>
-						<p className="About-p text-justify">Es una de las mejores opciones en tratamientos faciales, corporales y depilación, además te ofrecemos productos de la mejor calidad para el cuidado de tu piel.</p>
+					<div>
+						<h3 class="About-h3 hyphens-none">Garantia</h3>
+						<p className="About-p">Es una de las mejores opciones en tratamientos faciales, corporales y depilación, además te ofrecemos productos de la mejor calidad para el cuidado de tu piel.</p>
 					</div>
 				</div>
-				{/* Repite la misma estructura para los otros elementos */}
-				<div className="col d-flex align-items-start px-5">
-					<div className="text-body-emphasis d-inline-flex align-items-center justify-content-center flex-shrink-0 me-3">
+				<div class="col d-flex align-items-start px-5">
+					<div class="text-body-emphasis d-inline-flex align-items-center justify-content-center flex-shrink-0 me-3">
 						<img className="About-img" src={image2} alt="" />
 					</div>
-					<div className="vertical-line mx-3 align-self-stretch"></div>
-					<div className="d-flex flex-column justify-content-start">
-						<h3 className="About-h3">MISIÓN</h3>
-						<p className="About-p text-justify">Mejorar el bienestar de nuestros clientes con servicios de la más alta calidad. Nuestros especialistas están altamente calificados en cada uno de los tratamientos que ofrecemos.</p>
+					<div>
+						<h3 class="About-h3 hyphens-none">Mision</h3>
+						<p className="About-p">Nuestra misión es mejorar el bienestar de nuestros clientes con servicios de la más alta calidad. Nuestros especialistas están altamente calificados en cada uno de los tratamientos que ofrecemos.</p>
 					</div>
 				</div>
-				<div className="col d-flex align-items-start px-5">
-					<div className="text-body-emphasis d-inline-flex align-items-center justify-content-center flex-shrink-0 me-3">
+				<div class="col d-flex align-items-start px-5">
+					<div class="text-body-emphasis d-inline-flex align-items-center justify-content-center flex-shrink-0 me-3">
 						<img className="About-img" src={image3} alt="" />
 					</div>
-					<div className="vertical-line mx-3 align-self-stretch"></div>
-					<div className="d-flex flex-column justify-content-start">
-						<h3 className="About-h3">CALIDAD</h3>
-						<p className="About-p text-justify">Ofrecemos tratamientos faciales y corporales, de la mas alta calidad, servicio y resultados además te ofrecemos productos de la mejorar la calidad del cuidado de tu piel.</p>
+					<div>
+						<h3 class="About-h3 hyphens-none">Nosotros</h3>
+						<p className="About-p">Somos una de las mejores compañías en ofrecer tratamientos faciales y corporales, de la mas alta calidad, servicio y resultados además te ofrecemos productos de la mejorar la calidad del cuidado de tu piel.</p>
 					</div>
 				</div>
-				{/* ... */}
 			</div>
 		</section>
 	)
