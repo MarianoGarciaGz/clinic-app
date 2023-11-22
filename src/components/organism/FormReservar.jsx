366341b5f9dd67f161ed6c236d6e68b24bb649fc
OscarFuentes2027 <oscarfuentes2027@gmail.com>
2023-11-22 01:45:54 -0600

    mejora1

--- src/components/organism/FormReservar.jsx
+++ src/components/organism/FormReservar.jsx
@@ -22,11 +22,96 @@ const FormReservar = () => {
   });
 
 
+  const [telefonoError, setTelefonoError] = useState(false);
+  const [nombresError, setNombresError] = useState(false);
+  const [apellidosError, setApellidosError] = useState(false);
+  const [formValid, setFormValid] = useState(false);
+  const [horaSeleccionada, setHoraSeleccionada] = useState(false);
+  const [tratamientoSeleccionado, setTratamientoSeleccionado] = useState(false);
+
+
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
+    if (e.target.id === 'telefono') {
+      const regex = /^[0-9]*$/;
+  
+      if (regex.test(e.target.value) || e.target.value === '') {
+        if (e.target.value.length <= 10) {
+          setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+          });
+          setTelefonoError(false);
+        } else {
+          setTelefonoError(true);
+        }
+      } else {
+        setTelefonoError(true);
+      }
+    } else if (e.target.id === 'apellidos' || e.target.id === 'nombres') {
+      const regex = /^[A-Za-z\s]+$/;
+  
+      if (regex.test(e.target.value) || e.target.value === '') {
+        setFormData({
+          ...formData,
+          [e.target.id]: e.target.value,
+        });
+        setTelefonoError(false);
+        setNombresError(false);
+        setApellidosError(false);
+      } else {
+        if (e.target.id === 'apellidos') {
+          setApellidosError(true);
+          setNombresError(false);
+          setTelefonoError(false);
+        } else if (e.target.id === 'nombres') {
+          setNombresError(true);
+          setApellidosError(false);
+          setTelefonoError(false);
+        }
+      }
+    } else if (e.target.id === 'fecha') {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }else if (e.target.id === 'comentarios') {
+        setFormData({
+          ...formData,
+          [e.target.id]: e.target.value,
+        });
+      
+    } else if (e.target.id === 'hora') {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+      setHoraSeleccionada(e.target.value !== ''); // Actualiza el estado si se selecciona una hora
+    } else if (e.target.id === 'tratamiento') {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+      setTratamientoSeleccionado(e.target.value !== ''); // Actualiza el estado si se selecciona un tratamiento
+    }
+  
+
+    if (
+    formData.nombres !== '' &&
+    formData.apellidos !== '' &&
+    formData.telefono !== '' &&
+    !telefonoError &&
+    !nombresError &&
+    !apellidosError
+    // Agrega otras validaciones necesarias aquí
+  ) {
+    if (horaSeleccionada && tratamientoSeleccionado) {
+      setFormValid(true); // Habilita el botón si los campos están completos y no hay errores
+    } else {
+      setFormValid(false); // Deshabilita el botón si falta seleccionar hora o tratamiento
+    }
+  } else {
+    setFormValid(false); // Deshabilita el botón si algún campo está vacío o hay errores
+  }
   };
 
   const handleSubmit = async (e) => {
@@ -64,38 +149,33 @@ const FormReservar = () => {
         <form className="needs-validation" noValidate onSubmit={handleSubmit}>
           <div class="row g-3">
            
-            <div class="col-sm-4">
-            <label for="username" class="form-label">Nombre(s)</label>
-              <div class="input-group has-validation ">
-                <span class="input-group-text bg-white custom-border-color-orange"><img src={usuario} alt="nombre" className="img-form"/></span>
-                <input type="text" class="form-control custom-border-color-orange" id="nombres" placeholder="Nombre(s)" required="" onChange={handleChange}/>
-               <div class="invalid-feedback">
-                  Ingresa un nombre válido.
+            <div className="col-sm-4">
+              <label htmlFor="nombres" className="form-label">Nombre(s)</label>
+                <div className="input-group has-validation">{/* ... */}
+                  <input type="text" className={`form-control custom-border-color-orange ${ nombresError ? 'is-invalid' : '' }`}
+                  id="nombres" placeholder="Nombre(s)" required="" onChange={handleChange}/>
+                  {nombresError && (<div className="invalid-feedback">Ingresa un nombre válido.</div>)}
                 </div>
-              </div>
+          </div>
+
+            <div className="col-sm-4">
+              <label htmlFor="apellidos" className="form-label">Apellidos</label>
+                <div className="input-group has-validation">{/* ... */}
+                  <input type="text" className={`form-control custom-border-color-orange ${apellidosError ? 'is-invalid' : ''}`} 
+                  id="apellidos" placeholder="Apellidos" required="" onChange={handleChange}/>
+                  {apellidosError && (<div className="invalid-feedback">Ingresa un apellido válido.</div>)}
+                </div>
             </div>
 
-            <div class="col-sm-4">
-            <label for="username" class="form-label">Apellidos</label>
-              <div class="input-group has-validation ">
-                <span class="input-group-text bg-white custom-border-color-orange"><img src={usuario} alt="apellidos" className="img-form"/></span>
-                <input type="text" class="form-control custom-border-color-orange" id="apellidos" placeholder="Apellidos" required="" onChange={handleChange}/>
-              <div class="invalid-feedback">
-                  Ingresa un apellido válido.
-                </div>
-              </div>
+            <div className="col-sm-4">
+            <label htmlFor="telefono" className="form-label">Teléfono</label>
+            <div className="input-group has-validation">
+              <span className="input-group-text bg-white custom-border-color-orange"><img src={telefono} alt="telefono" className="img-form" /></span>
+              <input type="text" className={`form-control custom-border-color-orange ${telefonoError ? 'is-invalid' : ''}`} id="telefono"
+             placeholder="Teléfono" required="" onChange={handleChange}/>
+              {telefonoError && (<div className="invalid-feedback">Ingresa un teléfono válido.</div>)}
             </div>
-
-            <div class="col-sm-4">
-            <label for="username" class="form-label">Télefono</label>
-              <div class="input-group has-validation ">
-                <span class="input-group-text bg-white custom-border-color-orange"><img src={telefono} alt="telefono" className="img-form"/></span>
-                <input type="text" class="form-control custom-border-color-orange" id="telefono" placeholder="Teléfono" required=""/>
-              <div class="invalid-feedback">
-                  Ingresa un teléfono válido.
-                </div>
-              </div>
-            </div>
+          </div>
 
 
             <div className="col-sm-6">
@@ -117,7 +197,7 @@ const FormReservar = () => {
             <label htmlFor="username" className="form-label">Hora</label>
                 <div className="input-group has-validation">
                  <span className="input-group-text bg-white custom-border-color-orange"><img src={hora} alt="hora" className="img-form"/></span>
-                 <select className="form-select bg-white custom-border-color-orange" id="hora" required onChange={handleChange}>
+                 <select className={`form-select bg-white custom-border-color-orange ${horaSeleccionada ? '' : 'is-invalid'}`} id="hora" required onChange={handleChange}>
                     <option value="">Seleccionar...</option>
                     <option>14:00</option>
                     <option>15:00</option>
@@ -133,16 +213,14 @@ const FormReservar = () => {
                 <label htmlFor="username" className="form-label">Tratamiento</label>
                 <div className="input-group has-validation">
                  <span className="input-group-text bg-white custom-border-color-orange"><img src={tratamiento} alt="tratamiento" className="img-form"/></span>
-                 <select className="form-select bg-white custom-border-color-orange" id="tratamiento" required onChange={handleChange}>
+                 <select className={`form-select bg-white custom-border-color-orange ${tratamientoSeleccionado ? '' : 'is-invalid'}`} id="tratamiento" required onChange={handleChange}>
                     <option value="">Seleccionar...</option>
                     <option>Masaje Corporal</option>
                     <option>Maquillaje</option>
                     <option>Peinado</option>
                     <option>Laser</option>
                  </select>
-                 <div className="invalid-feedback">
-                    Selecciona una hora válida.
-                 </div>
+                 
                 </div>
              </div>
 
@@ -160,9 +238,16 @@ const FormReservar = () => {
 
           </div>
 
-            <div className="text-center">
-              <button className="btn btn-primary btn-lg-8 mt-4 w-50" type="submit">RESERVAR</button>
-            </div>
+          <div className="text-center">
+  <button
+    className={`btn btn-primary btn-lg-8 mt-4 w-50 ${formValid ? '' : 'disabled'}`}
+    type="submit"
+    style={{ backgroundColor: formValid ? '' : '#CCCCCC' }}
+    disabled={!formValid}
+  >
+    RESERVAR
+  </button>
+</div>
 
         </form>
         
