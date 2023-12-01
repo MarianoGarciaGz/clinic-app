e7506358d188a81ec3ccb36dfe9952c6948ac42e
chavezlopezabigail28 <chavezlopezabigail28@gmail.com>
2023-12-01 02:48:44 -0600

    Tratamientos: title, price, description

--- src/components/organism/CardTratamiento.jsx
+++ src/components/organism/CardTratamiento.jsx
@@ -1,118 +1,112 @@
-
-import React, { useEffect } from 'react';
-import '../../assets/css/style.css';
-import img from '../Images/person1.jpg';
-import Swiper from 'swiper';
+// CardTratamiento.jsx
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+import { cardsData } from '../atoms/data';  // Importa la lista de tarjetas
+import { cardsData2 } from '../atoms/data';  // Importa la lista de tarjetas
+import '../../assets/css/style.css';
 
-const CardTratamiento = () => {
-  useEffect(() => {
-    const initializeSwiper = () => {
-      new Swiper('.slide-content', {
-        slidesPerView: 3,
-        spaceBetween: 25,
-        loop: true,
-        centerSlide: 'true',
-        fade: 'true',
-        grabCursor: 'true',
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-        },      
-      });
-    };
-
-    const swiperScript = document.createElement('script');
-    swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@11.0.5/swiper-bundle.min.js';
-    swiperScript.async = true;
-    document.body.appendChild(swiperScript);
-
-    const swiperStylesheet = document.createElement('link');
-    swiperStylesheet.href = 'https://cdn.jsdelivr.net/npm/swiper@11.0.5/swiper-bundle.min.css';
-    swiperStylesheet.rel = 'stylesheet';
-    document.head.appendChild(swiperStylesheet);
-
-    swiperScript.onload = initializeSwiper;
-
-    return () => {
-      document.body.removeChild(swiperScript);
-      document.head.removeChild(swiperStylesheet);
-    };
-  }, []);
-
+export const CardTratamiento = () => {
   return (
-
-    <div className='slide-container swiper'>
-      <div className="slide-content">
-        <div className="card-wrapper swiper-wrapper">
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={40}
+      loop={true}
+      grabCursor={true}
+      pagination={{
+        clickable: true,
+        dynamicBullets: true,
+      }}
+      navigation={true}
+      breakpoints={{
+        0: {
+          slidesPerView: 1,
+        },
+        520: {
+          slidesPerView: 2,
+        },
+        950: {
+          slidesPerView: 3,
+        },
+      }}
+      modules={[Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {cardsData.map((card, index) => (
+        <SwiperSlide key={index}>
           <div className="card swiper-slide">
             <div className="image-content">
               <span className="overlay"></span>
               <div className="card-image">
-                <img src={img} alt="" className="card-img"></img>
-              </div>
-
-            </div>
-            <div className="card-content">
-              <h2 className="name">David Dell</h2>
-              <p className="desciption">Esto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas información</p>
-              <button className="button">Reservar</button>
-            </div>
-          </div>
-          <div className="card swiper-slide">
-            <div className="image-content">
-              <span className="overlay"></span>
-              <div className="card-image">
-                <img src={img} alt="" className="card-img"></img>
-              </div>
-
-            </div>
-            <div className="card-content">
-              <h2 className="name">David Dell</h2>
-              <p className="desciption">Esto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas información</p>
-              <button className="button">Reservar</button>
-            </div>
-          </div>
-          <div className="card swiper-slide">
-            <div className="image-content">
-              <span className="overlay"></span>
-              <div className="card-image">
-                <img src={img} alt="" className="card-img"></img>
-              </div>
-
-            </div>
-            <div className="card-content">
-              <h2 className="name">David Dell</h2>
-              <p className="desciption">Esto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas información</p>
-              <button className="button">Reservar</button>
-            </div>
-          </div>
-          <div className="card swiper-slide">
-            <div className="image-content">
-              <span className="overlay"></span>
-              <div className="card-image">
-                <img src={img} alt="" className="card-img"></img>
+                <img src={card.image} alt="" className="card-img" />
               </div>
             </div>
             <div className="card-content">
-              <h2 className="name">David Dell</h2>
-              <p className="desciption">Esto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas informaciónEsto es una prueba no se que estoy haciendo pero funciona, jajajaja y pondre mas información</p>
+              <h2 className="name">{card.name}</h2>
+              <h3 className="precio">${card.precio}</h3>
+              <p className="description" style={{ whiteSpace: 'pre-line' }}>
+                {card.description}
+              </p>
               <button className="button">Reservar</button>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className="swiper-pagination"></div>
-
-    </div>
-
-
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
-export default CardTratamiento;
-
-
+export const CardTratamientoCorporales = () => {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={40}
+      loop={true}
+      grabCursor={true}
+      pagination={{
+        clickable: true,
+        dynamicBullets: true,
+      }}
+      navigation={true}
+      breakpoints={{
+        0: {
+          slidesPerView: 1,
+        },
+        520: {
+          slidesPerView: 2,
+        },
+        950: {
+          slidesPerView: 3,
+        },
+      }}
+      modules={[Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {cardsData2.map((card, index) => (
+        <SwiperSlide key={index}>
+          <div className="card swiper-slide">
+            <div className="image-content">
+              <span className="overlay"></span>
+              <div className="card-image">
+                <img src={card.image2} alt="" className="card-img" />
+              </div>
+            </div>
+            <div className="card-content">
+              <h2 className="name">{card.name2}</h2>
+              <h3 className="precio">${card.precio2}</h3>
+              <p className="description" style={{ whiteSpace: 'pre-line' }}>
+                {card.description2}
+              </p>
+              <button className="button">Reservar</button>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
