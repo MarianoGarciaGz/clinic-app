778fef3134863762d4f9bed1430c136663c32bd1
Andrea_Aguilar <l20120949@morelia.tecnm.mx>
2023-12-04 09:44:16 -0600

    Galeria terminada

--- src/components/organism/GalleryClinic.jsx
+++ src/components/organism/GalleryClinic.jsx
@@ -1,15 +1,79 @@
 import React, { useState } from 'react'
-import img1 from '../Images/cb1.jpg'
-import img2 from '../Images/cb2.jpg'
-import img3 from '../Images/cb3.jpg'
-import img4 from '../Images/cb4.jpg'
-import img5 from '../Images/cb5.jpg'
-import img6 from '../Images/cb6.jpg'
-import img7 from '../Images/cb7.jpg'
-import img8 from '../Images/cb8.jpg'
-import img9 from '../Images/cb9.jpg'
-import img10 from '../Images/cb10.jpg'
-import logo from '../Images/logon.png'
+
+/* FOTOS CABINA CORPORALES 2*/ 
+import img1 from '../Images/C1.jpg'
+import img2 from '../Images/C2.jpg'
+import img3 from '../Images/C3.jpg'
+import img4 from '../Images/C4.jpg'
+import img5 from '../Images/C5.jpg'
+import img6 from '../Images/C6.jpg'
+import img7 from '../Images/C7.jpg'
+import img8 from '../Images/C8.jpg'
+import img9 from '../Images/C9.jpg'
+import img10 from '../Images/C10.jpg'
+
+/* FOTOS CABINA FACIALES*/ 
+import img11 from '../Images/F1.jpg'
+import img12 from '../Images/F2.jpg'
+import img13 from '../Images/F3.jpg'
+import img14 from '../Images/F4.jpg'
+import img15 from '../Images/F5.jpg'
+import img16 from '../Images/F6.jpg'
+import img17 from '../Images/F7.jpg'
+import img18 from '../Images/F8.jpg'
+import img19 from '../Images/F9.jpg'
+import img20 from '../Images/F10.jpg'
+
+/* FOTOS CABINA LASER*/ 
+import img21 from '../Images/L1.jpg'
+import img22 from '../Images/L2.jpg'
+import img23 from '../Images/L3.jpg'
+import img24 from '../Images/L4.jpg'
+import img25 from '../Images/L5.jpg'
+import img26 from '../Images/L6.jpg'
+import img27 from '../Images/L7.jpg'
+import img28 from '../Images/L8.jpg'
+import img29 from '../Images/L9.jpg'
+
+/* FOTOS CABINA MASAJES*/ 
+import img30 from '../Images/M1.jpg'
+import img31 from '../Images/M2.jpg'
+import img32 from '../Images/M3.jpg'
+import img33 from '../Images/M4.jpg'
+import img34 from '../Images/M5.jpg'
+import img35 from '../Images/M6.jpg'
+import img36 from '../Images/M7.jpg'
+import img37 from '../Images/M8.jpg'
+import img38 from '../Images/M9.jpg'
+import img39 from '../Images/M10.jpg'
+
+/*FOTOS SALON ROSITA*/
+import img40 from '../Images/SR1.jpg'
+import img41 from '../Images/SR2.jpg'
+import img42 from '../Images/SR3.jpg'
+import img43 from '../Images/SR4.jpg'
+import img44 from '../Images/SR5.jpg'
+import img45 from '../Images/SR6.jpg'
+import img46 from '../Images/SR7.jpg'
+import img47 from '../Images/SR8.jpg'
+import img48 from '../Images/SR9.jpg'
+import img49 from '../Images/SR10.jpg'
+import img50 from '../Images/SR11.jpg'
+import img51 from '../Images/SR12.jpg'
+
+/*FOTOTS CORPORALES 1*/
+import img52 from '../Images/TC1.jpg'
+import img53 from '../Images/TC2.jpg'
+import img54 from '../Images/TC3.jpg'
+import img55 from '../Images/TC4.jpg'
+import img56 from '../Images/TC5.jpg'
+import img57 from '../Images/TC6.jpg'
+import img58 from '../Images/TC7.jpg'
+import img59 from '../Images/TC8.jpg'
+import img60 from '../Images/TC9.jpg'
+
+
+
 
 const Gallery = () => {
 
@@ -31,16 +95,75 @@ const Gallery = () => {
   });
 
 	const images = [
-		{ category: 'sala-1', src: img1, alt: 'img' },
+		/*CABINA TRATAMIENTOS CORPORALES*/
+		{ category: 'sala-1', src: img52, alt: 'img' },
+		{ category: 'sala-1', src: img54, alt: 'img' },
+		{ category: 'sala-1', src: img55, alt: 'img' },
+		{ category: 'sala-1', src: img56, alt: 'img' },
+		{ category: 'sala-1', src: img59, alt: 'img' },
+		{ category: 'sala-1', src: img60, alt: 'img' },
+		{ category: 'sala-1', src: img53, alt: 'img' },
+		{ category: 'sala-1', src: img57, alt: 'img' },
+		{ category: 'sala-1', src: img58, alt: 'img' },
+		
+		/*CABINA TRATAMIENTOS CORPORALES*/
 		{ category: 'sala-2', src: img2, alt: 'img' },
-		{ category: 'sala-3', src: img3, alt: 'img' },
-		{ category: 'sala-4', src: img4, alt: 'img' },
-		{ category: 'sala-5', src: img5, alt: 'img' },
-		{ category: 'sala-1', src: img6, alt: 'img' },
+		{ category: 'sala-2', src: img10, alt: 'img' },
+		{ category: 'sala-2', src: img8, alt: 'img' },
+		{ category: 'sala-2', src: img9, alt: 'img' },
+		{ category: 'sala-2', src: img1, alt: 'img' },
+		{ category: 'sala-2', src: img6, alt: 'img' },
 		{ category: 'sala-2', src: img7, alt: 'img' },
-		{ category: 'sala-3', src: img8, alt: 'img' },
-		{ category: 'sala-4', src: img9, alt: 'img' },
-		{ category: 'sala-5', src: img10, alt: 'img' },
+		{ category: 'sala-2', src: img4, alt: 'img' },
+		{ category: 'sala-2', src: img9, alt: 'img' },
+		
+		
+		/*CABINA FACIALES*/
+		{ category: 'sala-3', src: img11, alt: 'img' },
+		{ category: 'sala-3', src: img12, alt: 'img' },
+		{ category: 'sala-3', src: img13, alt: 'img' },
+		{ category: 'sala-3', src: img14, alt: 'img' },
+		{ category: 'sala-3', src: img15, alt: 'img' },
+		{ category: 'sala-3', src: img16, alt: 'img' },
+		{ category: 'sala-3', src: img17, alt: 'img' },
+		{ category: 'sala-3', src: img18, alt: 'img' },
+		{ category: 'sala-3', src: img19, alt: 'img' },
+
+		/*CABINA DE MASAJES*/
+		{ category: 'sala-4', src: img30, alt: 'img' },
+		{ category: 'sala-4', src: img32, alt: 'img' },
+		{ category: 'sala-4', src: img33, alt: 'img' },
+		{ category: 'sala-4', src: img34, alt: 'img' },
+		{ category: 'sala-4', src: img35, alt: 'img' },
+		{ category: 'sala-4', src: img36, alt: 'img' },
+		{ category: 'sala-4', src: img37, alt: 'img' },
+		{ category: 'sala-4', src: img38, alt: 'img' },
+		{ category: 'sala-4', src: img39, alt: 'img' },
+
+		/*CABINA DE LASER*/	
+		{ category: 'sala-5', src: img22, alt: 'img' },
+		{ category: 'sala-5', src: img28, alt: 'img' },
+		{ category: 'sala-5', src: img23, alt: 'img' },
+		{ category: 'sala-5', src: img24, alt: 'img' },
+		{ category: 'sala-5', src: img25, alt: 'img' },
+		{ category: 'sala-5', src: img26, alt: 'img' },
+		{ category: 'sala-5', src: img21, alt: 'img' },
+		{ category: 'sala-5', src: img29, alt: 'img' },
+		{ category: 'sala-5', src: img21, alt: 'img' },
+
+		/*SALON ROSTITA*/
+		{ category: 'sala-6', src: img40, alt: 'img' },
+		{ category: 'sala-6', src: img44, alt: 'img' },
+		{ category: 'sala-6', src: img49, alt: 'img' },
+		{ category: 'sala-6', src: img41, alt: 'img' },
+		{ category: 'sala-6', src: img45, alt: 'img' },
+		{ category: 'sala-6', src: img43, alt: 'img' },
+		{ category: 'sala-6', src: img42, alt: 'img' },
+		{ category: 'sala-6', src: img46, alt: 'img' },
+		{ category: 'sala-6', src: img47, alt: 'img' },
+		{ category: 'sala-6', src: img48, alt: 'img' },
+		{ category: 'sala-6', src: img50, alt: 'img' },
+		{ category: 'sala-6', src: img43, alt: 'img' },
 	]
 
 	
